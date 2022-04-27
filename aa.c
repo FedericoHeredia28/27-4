86fdf762fa8a3344b0c2675dabc63db4605af94e
Federico Heredia <federicomartinheredia2006@gmail.com>
2022-04-27 17:49:40 -0300

    a

---
+++ aa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int suma = 0;
+  int max = 0;
+  int min = 100;
+for (int i = 0; i < 20; i++) {
+  int r = rand() % 100;
+  if (n > max) max = n;
+  if (n < min) min = n;
+  suma += n;
+  printf("%d", r);
+}
+printf("\n");
+printf("prom: %.2f\n", suma/20.0;
+printf("max: %d\n", max);
+printf("min: %d\n", min);
+  return 0;
+}
